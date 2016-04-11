1571403375990c0798659df0cf5a14fc097bbd8d
Veniamin Zinevych <mitchsvik@gmail.com>
2016-04-11 17:50:09 +0300

    version 0.2
    +slicing array for parallel calculation;

--- taskTcpServer/server.js
+++ taskTcpServer/server.js
@@ -3,19 +3,27 @@ global.api = api;
 api.net = require('net');
 
 var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+var sockets = [];
 var results = [];
 
 var server = api.net.createServer(function(socket) {
+  sockets.push(socket);
   console.log('Connected: ' + socket.localAddress);
-  socket.write(JSON.stringify(task));
-  console.log('Data sended (by server): ' + task)
+  console.log('Current connected : ' + sockets.length + ' clients');
+  results = [];
+    
+  var size = task.length/sockets.length;
+  sockets.forEach(function(socket, index) {
+    socket.write(JSON.stringify(task.slice(size*index, size*(index+1))));
+    console.log('Data sended (by server): ' + task.slice(size*index, size*(index+1)))          
+  });
   
   socket.on('data', function(data) {
-    var result = JSON.parse(data);
-    console.log('Data resiever (by server): ' + result);
+    results.push(JSON.parse(data));
+    console.log('Data resiever (by server): ' + results);
   });
 
   socket.on('end', function(){
-     console.log('User disconnected\n'); 
+    console.log('User disconnected\n'); 
   });
 }).listen(2000);
